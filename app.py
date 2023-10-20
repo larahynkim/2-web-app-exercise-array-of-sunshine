af70e67fcf122b638260ff3b7ab3360ee83576ce
lc <1877608896@qq.com>
2023-10-20 18:32:29 -0400

    calendar back end done

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ database = client.get_database('mindscape')
 users = database.get_collection('users') 
 
 
-
 @app.route('/')
 def start():
     return render_template('login.html')
@@ -23,7 +22,7 @@ def login():
     if user:
         # Check if the provided password matches the stored hashed password
         if password == user['password']:
-            return render_template('home_page.html')
+            return render_template('home_page.html', current_user=username)
         else:
             return "Login Failed (Invalid Password)"
     else:
@@ -46,24 +45,41 @@ def create_new_account():
             'password': password
         }
         users.insert_one(user_data)
+        database.create_collection(str(username)+'_calendar') 
         return "Registration Successful"
     else:
         return "Registration Failed (Username already in use)"
 
-@app.route('/home')
-def home():
-    return render_template('home_page.html')
+@app.route('/home/<parameter>')
+def home(parameter):
+    return render_template('home_page.html', current_user = parameter)
 
-@app.route('/entries')
-def entries():
-    return render_template('entries.html')
+@app.route('/entries/<parameter>')
+def entries(parameter):
+    return render_template('entries.html',current_user = parameter)
 
-@app.route('/calendar')
-def calendar():
-    return render_template('calendar.html')
+@app.route('/calendar/<parameter>')
+def calendar(parameter):
+    dates = []
+    cursor = database[str(parameter) + '_calendar'].find({}, {"date": 1, "_id": 0})
+    for doc in cursor:
+        dates.append(doc["date"])
+    return render_template('calendar.html', current_user = parameter, checked_dates=dates)
 
-@app.route('/account')
-def account():
+@app.route('/checkin/<parameter>', methods=["GET", "POST"])
+def checkin(parameter):
+    if request.method == "POST":
+        if not database[str(parameter)+'_calendar'].find_one({'date': request.form["date"]}):
+            database[str(parameter)+'_calendar'].insert_one({"date": request.form["date"]})
+    dates = []
+    cursor = database[str(parameter) + '_calendar'].find({}, {"date": 1, "_id": 0})
+    for doc in cursor:
+        dates.append(doc["date"])
+    return render_template("calendar.html", current_user = parameter, checked_dates=dates)
+
+@app.route('/account/<parameter>')
+def account(parameter):
+    #not done
     return render_template('account.html')
 
 if __name__ == '__main__':
